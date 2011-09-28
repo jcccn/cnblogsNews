ebe08a4681f7485baac23e52abbf116dbab9a3e3
jcccn <jccuestc@gmail.com>
2011-09-28 21:30:33 +0800

    Initial Commit

---
+++ cnblogsNews/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  cnblogsNews
+//
+//  Created by Jiang Chuncheng on 9/28/11.
+//  Copyright 2011 SenseForce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ cnblogsNews/cnblogsNewsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  cnblogsNewsAppDelegate.h
+//  cnblogsNews
+//
+//  Created by Jiang Chuncheng on 9/28/11.
+//  Copyright 2011 SenseForce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cnblogsNewsAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
