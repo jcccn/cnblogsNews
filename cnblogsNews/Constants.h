5cddd9cb309397d04a60dcfb695ed08ae0a3e01e
jcccn <jccuestc@gmail.com>
2013-11-17 00:35:22 +0800

    add sns app keys

--- cnblogsNews/Constants.h
+++ cnblogsNews/Constants.h
@@ -9,6 +9,33 @@
 #ifndef cnblogsNews_Constants_h
 #define cnblogsNews_Constants_h
 
+#define AppId               @"468638662"
+#define AppStoreUrl         @"https://itunes.apple.com/cn/app/bo-ke-yuan-xin-wen/id468638662"
+#define AppStoreShortUrl    @"http://t.cn/8DsEjOq"
+
+#define UmengAppKey             @"senseforce.com"
+
+#define ShareSDKAppKey          @"348087c7c02"
+
+#define SinaWeiboAppKey         @"2257674641"
+#define SinaWeiboAppSecret      @"cda0e4d654730dd4599f75eea19e4b89"
+#define SinaWeiboCallbackUrl    @"http://www.senseforce.com"
+
+#define TencentWeiboAppKey      @"801444280"
+#define TencentWeiboAppSecret   @"f4e16b85c9d7825226791810ba85f9cc"
+#define TencentWeiboCallbackUrl @"http://www.senseforce.com"
+
+#define WeChatAppKey            @"wx89ac0e84230e7cdd"
+
+#define QZoneAppKey             @"100558177"
+#define QZoneAppSecret          @"ed23f66bf1e7bbdff21eafe046f88070"
+
+#define YinXiangAppKey          @"jcccn-1934"
+#define YinXiangAppSecret       @"ad129b251f2e9d96"
+
+#define PocketAppSecret         @"20460-55755e22daab5986c555144d"
+#define PocketCallbackUrl       @"pocketapp20460"
+
 #pragma mark -
 #pragma mark Umeng MobClick
 #define MobClickAppKey                  @"senseforce.com"
@@ -20,6 +47,6 @@
 #define MobClickEventIdClickNextPage    @"ClickNextPage"
 #define MobClickEventIdClickInfoButton  @"ClickInfoButton"
 
-#define iAdShowFlag     YES
+#define iAdShowFlag     NO
 
 #endif
